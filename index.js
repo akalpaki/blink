bbf0229423f2725c9b4463987e19a5e8fb9f9340
Alex Kalpakidis <akalpaki@proton.me>
2025-01-25 00:46:28 +0200

    working on understanding child processes for curling

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { argv } from 'node:process';
+import { spawn } from 'node:child_process';
 
 const getArgs = () =>
     argv.reduce((args, arg) => {
@@ -23,4 +24,20 @@ const getArgs = () =>
 
 const args = getArgs();
 
+console.log(args)
 
+const curl = spawn("curl", { url: args.url });
+
+console.log(curl)
+
+curl.stdout.on('data', (data) => {
+    console.log(data);
+});
+
+curl.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+});
+
+curl.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+});
