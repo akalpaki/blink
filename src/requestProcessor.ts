4cd9eb29888b93ae2196cc50da3367b280286b6d
Alex Kalpakidis <akalpaki@proton.me>
2025-02-11 22:54:34 +0200

    working on request processor

--- src/requestProcessor.ts
+++ src/requestProcessor.ts
@@ -1,13 +1,84 @@
-import { DatabaseSync } from "node:sqlite"
+import Database from "better-sqlite3"
+
+import { DataModel, RequestOptions, Response } from "./types";
+import { doFetch } from "./curl";
 
 export class RequestProcessor {
-    #conn; // private connection handler
+    private conn; // private connection handler
 
     constructor(storeLocation = ":memory:") {
         if (storeLocation === ":memory:") {
             console.log("Running in :memory: mode!")
         }
-        this.#conn = new DatabaseSync(storeLocation)
+        this.conn = new Database(storeLocation)
+        this.conn.pragma("journal_mode = WAL")
+        this.setupDatabase()
+    }
+
+    public async doRequest(req: RequestOptions): Promise<Response> {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const data: DataModel = {
+                    url: req.url.toString(),
+                    method: "",
+                }
+
+                if (typeof req.name !== "undefined") {
+                    data.name = req.name;
+                }
+
+                if (typeof req.method !== "undefined") {
+                    data.method = req.method
+                }
+
+                if (typeof req.headers !== "undefined") {
+                    data.headers = req.headers
+                }
+
+                if (typeof req.body !== "undefined") {
+                    data.body = req.body
+                }
+
+                const id = this.saveData(data)
+                req.id = id as bigint
+
+                const res = await doFetch(req);
+
+                resolve(res);
+            } catch (err) {
+                reject(err)
+            };
+        })
+    }
+
+    private setupDatabase() {
+        this.conn.exec(`
+CREATE TABLE IF NOT EXISTS requests(
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+name TEXT,
+url TEXT NOT NULL,
+method TEXT,
+headers TEXT,
+body TEXT,
+mediaType TEXT,
+created_at TEXT,
+updated_at TEXT
+);`)
+    }
+
+    private saveData(data: DataModel): number | bigint {
+        const stmt = this.conn.prepare(`INSERT INTO requests(name,url,method,headers,body,mediaType) VALUES (?,?,?,?,?,?) RETURNING id`)
+
+        const res = stmt.run(
+            data.name,
+            data.url,
+            data.method,
+            data.headers,
+            data.body,
+            data.mediaType,
+        )
+
+        return res.lastInsertRowid
     }
 
 }
--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,6 @@
 export interface RequestOptions {
+    id?: bigint,
+    name?: string
     method?: string,
     url: URL,
     headers?: HeadersInit,
@@ -6,8 +8,21 @@ export interface RequestOptions {
 }
 
 export interface Response {
+    id?: bigint,
     status: number,
     headers: Headers,
     mediaType: string,
     body: any | null,
+}
+
+export interface DataModel {
+    id?: bigint,
+    name?: string,
+    url: string,
+    method?: string,
+    headers?: Headers,
+    body?: any | null,
+    mediaType?: string,
+    created_at?: string,
+    updated_at?: string
 }
